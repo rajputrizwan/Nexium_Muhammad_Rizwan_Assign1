1ebb77b223938c66d3bf4c08e34ef838c28e6a39
Muhammad Rizwan <mrizwan2702@gmail.com>
2025-07-07 19:36:30 +0500

    fix-deployment-4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,13 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends("next/core-web-vitals", "next"),
   {
     rules: {
       "react/no-unescaped-entities": "off",
     },
   },
 ];
-
-export default eslintConfig;
